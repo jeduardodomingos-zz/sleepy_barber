780bd164871f82d0e521a6ce01a0e1a35e553964
jeduardodomingos <juan.eduardo@outlook.com.br>
2019-05-19 15:46:45 -0300

    Primeiro commit

---
+++ cmake-build-debug/CMakeFiles/sleepy_barber.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sleepy_barber.dir/main.c.obj"
+  "sleepy_barber.pdb"
+  "sleepy_barber.exe"
+  "sleepy_barber.exe.manifest"
+  "sleepy_barber.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sleepy_barber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
