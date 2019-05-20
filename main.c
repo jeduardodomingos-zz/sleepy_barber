0d71e15f83008a0e32b7dfeee81b814fe6521fbb
jeduardodomingos <juan.eduardo@outlook.com.br>
2019-05-19 22:22:27 -0300

    Adiconada a contagem de processos + Criação de processos + Funções de Liberação na conclusão do main

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 #define DEFAULT_PAUSE 1
 #define MILLISECOND 1000
 
+//Para as variáveis inteiras utilizadas como booleanas, consideremos 1 para TRUE e 0 para FALSE
+
 struct chair {
     int chairNumber;
     int isBusy;
@@ -17,30 +19,67 @@ struct barber {
     int isBusy;
 };
 
+struct customer {
+    int customerNumber;
+    int isWaiting;
+    int isAttend;
+    int alreadyAttended
+};
+
+
 typedef struct chair chairStructure;
 typedef struct barber barberStructure;
+typedef struct customer customerStructure;
+
 
 void presentation();
 int getMaxChairNumber();
 int getProcessNumberToEntry();
+int getProcessNumberToEntry();
+int countWaitingCustomers(customerStructure *customers);
+int countAttendCustomers(customerStructure *customers);
+int countAlreadyAttendCustomers(customerStructure *customers);
+int countTotalCustomers(customerStructure *customers);
 
 int main() {
-    chairStructure *avaliableChairs;
-    barberStructure*avaliableBarbers;
+    chairStructure *availableChairs = NULL;
+    barberStructure *availableBarbers = NULL;
+    customerStructure *customers = NULL;
 
     pthread_t threadOne;
     pthread_t threadTwo;
 
     presentation();
 
-    avaliableChairs = (chairStructure*) malloc(sizeof(chairStructure) * getMaxChairNumber()); //O usuário definirá quantas cadeiras de espera estarão disponíveis
-    avaliableBarbers = (barberStructure*) malloc(sizeof(barberStructure) * 2); //haverão dois Barbeiros (Thread Disponíveis);
+    availableChairs = (chairStructure *) malloc(sizeof(chairStructure) * getMaxChairNumber()); //O usuário definirá quantos slots de espera estarão disponíveis
+    availableBarbers = (barberStructure *) malloc(sizeof(barberStructure) * 2); //haverão duas threads disponíveis (Simulando os Barbeiros)
 
-    
+
+    free(availableChairs);
+    free(availableBarbers);
+    free(customers);
+
     return 0;
 }
 
-void presentation(){
+void includeCustomer(customerStructure **customers){
+    customerStructure newCustomer;
+
+    newCustomer.customerNumber = countTotalCustomers(*customers);
+    newCustomer.alreadyAttended = 0;
+    newCustomer.isAttend = 0;
+    newCustomer.isWaiting = 0;
+
+    if((*customers) == NULL){
+        *customers = malloc(sizeof(customerStructure));
+    }else{
+        *customers = realloc(customers, sizeof(*customers) + sizeof(customerStructure));
+    }
+
+    *customers[(sizeof(*customers) / sizeof(customerStructure)) - 1] = newCustomer;
+}
+
+void presentation() {
     system("cls");
     Sleep(DEFAULT_PAUSE * MILLISECOND);
     printf("----------------------- Seja bem vindo a simulacao de gerenciamento de processos ---------------------");
@@ -51,7 +90,7 @@ void presentation(){
     system("cls");
 }
 
-int getMaxChairNumber(){
+int getMaxChairNumber() {
     int chairs = 0;
 
     printf("Qual sera o numero de cadeiras disponiveis ?\n");
@@ -59,9 +98,9 @@ int getMaxChairNumber(){
     scanf("%d", &chairs);
 
     return chairs;
-};
+}
 
-int getProcessNumberToEntry(){
+int getProcessNumberToEntry() {
     int processes = 0;
 
     system("cls");
@@ -70,4 +109,50 @@ int getProcessNumberToEntry(){
     scanf("%d", &processes);
 
     return processes;
+}
+
+int countWaitingCustomers(customerStructure *customers) {
+    int waitingCustomers = 0;
+
+    for (int i = 0; i < (sizeof(customers) / sizeof(customerStructure)); i++) {
+        if (customers[i].isWaiting) {
+            waitingCustomers++;
+        }
+    }
+
+    return waitingCustomers;
+}
+
+int countAttendCustomers(customerStructure *customers){
+    int attendCustomers = 0;
+
+    for (int i = 0; i < (sizeof(customers) / sizeof(customerStructure)); i++) {
+        if (customers[i].isAttend) {
+            attendCustomers++;
+        }
+    }
+
+    return attendCustomers;
+}
+
+int countAlreadyAttendCustomers(customerStructure *customers){
+    int alreadyAttendCustomers = 0;
+
+    for (int i = 0; i < (sizeof(customers) / sizeof(customerStructure)); i++) {
+        if (customers[i].alreadyAttended) {
+            alreadyAttendCustomers++;
+        }
+    }
+
+    return alreadyAttendCustomers;
+}
+
+int countTotalCustomers(customerStructure *customers){
+    int totalCustomers = 0;
+
+    for (int i = 0; i < (sizeof(customers) / sizeof(customerStructure)); i++) {
+        totalCustomers++;
+    }
+
+    return totalCustomers;
 }
