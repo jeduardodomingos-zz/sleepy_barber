7b866086ae3e24e0e3ba3bb6e15774d8caaf8f52
jeduardodomingos <juan.eduardo@outlook.com.br>
2019-05-20 12:50:40 -0300

    Implementado o método para servir as threads + controle de threads disponíveis

--- main.c
+++ main.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <Windows.h>
 #include <pthread.h>
+#include <time.h>
 
 #define DEFAULT_PAUSE 1
+#define ATTEND_PAUSE 2
 #define MILLISECOND 1000
 
 //Para as variáveis inteiras utilizadas como booleanas, consideremos 1 para TRUE e 0 para FALSE
@@ -31,61 +33,74 @@ typedef struct chair chairStructure;
 typedef struct barber barberStructure;
 typedef struct customer customerStructure;
 
+void includeCustomer(customerStructure **customers);
 void presentation();
-int getMaxChairNumber();
-int getProcessNumberToEntry();
-int getProcessNumberToEntry();
+int setMaxChairNumber(int *chairs);
 int countWaitingCustomers(customerStructure *customers);
 int countAttendCustomers(customerStructure *customers);
 int countAlreadyAttendCustomers(customerStructure *customers);
 int countTotalCustomers(customerStructure *customers);
 int navigationMenu(customerStructure *customers);
+void writeInLogFile(char operation[200], int customerNumber);
+void * processCustomer(void *args);
+void operationResume(customerStructure *customers);
 
 int main() {
     int selectedOption = 0;
+    int availableChairNumber = 0;
 
-    chairStructure *availableChairs = NULL;
-    barberStructure *availableBarbers = NULL;
     customerStructure *customers = NULL;
 
     pthread_t threadOne;
     pthread_t threadTwo;
 
     presentation();
+    setMaxChairNumber(&availableChairNumber); //O usuário definirá quantos slots de espera estarão disponíveis
 
-    availableChairs = (chairStructure *) malloc(sizeof(chairStructure) * getMaxChairNumber()); //O usuário definirá quantos slots de espera estarão disponíveis
-    availableBarbers = (barberStructure *) malloc(sizeof(barberStructure) * 2); //haverão duas threads disponíveis (Simulando os Barbeiros)
+    pthread_create(&threadOne, NULL, processCustomer, (void*) customers);
+    pthread_create(&threadTwo, NULL, processCustomer, (void*) customers);
+
+    pthread_join(threadOne, NULL);
+    pthread_join(threadTwo, NULL);
 
     while(selectedOption != 3){
         selectedOption = navigationMenu(customers);
 
         switch (selectedOption){
             case 1:
-                //TODO - Incluir novo Cliente
+                includeCustomer(&customers);
                 break;
             case 2:
-                //TODO - Exibir Log de Execucoes
+                operationResume(customers);
                 break;
+            case 3:
+                system("cls");
+                printf("A execução do sistema está sendo finalizada");
+                Sleep(DEFAULT_PAUSE * MILLISECOND);
             default:
-                //TODO - Opção Inválida voltar para o inicio
+                system("cls");
+                printf("Opcao invalida ...");
+                Sleep(DEFAULT_PAUSE * MILLISECOND);
                 break;
         }
     }
 
-    free(availableChairs);
-    free(availableBarbers);
     free(customers);
 
     return 0;
 }
 
-void includeCustomer(customerStructure **customers){
+void includeCustomer(customerStructure **customers, int availableChairs){
     customerStructure newCustomer;
 
     newCustomer.customerNumber = countTotalCustomers(*customers) + 1;
     newCustomer.alreadyAttended = 0;
     newCustomer.isAttend = 0;
     newCustomer.isWaiting = 0;
+
+    if(countWaitingCustomers(*customers) >= availableChairs){
+        
+    }
 
     if((*customers) == NULL){
         *customers = malloc(sizeof(customerStructure));
@@ -107,25 +122,10 @@ void presentation() {
     system("cls");
 }
 
-int getMaxChairNumber() {
-    int chairs = 0;
-
+int setMaxChairNumber(int *chairs) {
     printf("Qual sera o numero de cadeiras disponiveis ?\n");
     printf("R:");
-    scanf("%d", &chairs);
-
-    return chairs;
-}
-
-int getProcessNumberToEntry() {
-    int processes = 0;
-
-    system("cls");
-    printf("Qual o numero de processos que deverao ser atendidos ?\n");
-    printf("R: ");
-    scanf("%d", &processes);
-
-    return processes;
+    scanf("%d", chairs);
 }
 
 int countWaitingCustomers(customerStructure *customers) {
@@ -188,3 +188,60 @@ int navigationMenu(customerStructure *customers){
 
     return selectedOption;
 }
+
+void writeInLogFile(char operation[200], int customerNumber){
+    char message[500];
+    char customer[10];
+    FILE *archiveReference;
+
+    itoa(customerNumber, customer, sizeof(customer) * 2);
+
+    archiveReference = fopen("c:/log/sleepy_barber.log", "w");
+
+    if(archiveReference == NULL){
+        printf("\n\nErro ao realizar abertura do arquivo para escrita.");
+        return;
+    }
+
+    strcat(message, __DATE__);
+    strcat(message, " ");
+    strcat(message, "Cliente de Numero: ");
+    strcat(message, customer);
+    strcat(message, operation);
+    strcat(message, "\n");
+
+    fprintf(archiveReference, "%s", message);
+
+    fclose(archiveReference);
+}
+
+void *processCustomer(void *args){
+    customerStructure *customers = (customerStructure*) args;
+
+    if(customers != NULL){
+        if(countWaitingCustomers(customers) > 0){
+            for(int i = 0; i < (sizeof(customers) / sizeof(customerStructure)); i++){
+                if(customers[i].alreadyAttended != 1){
+                    customers[i].isAttend = 1;
+                    customers[i].isWaiting = 0;
+                    writeInLogFile("EM ATENDIMENTO", customers[i].customerNumber);
+                    Sleep(ATTEND_PAUSE * MILLISECOND);
+                }else{
+                    continue;
+                }
+            }
+        }
+    }
+}
+
+void operationResume(customerStructure *customers){
+    system("cls");
+    printf("\n\n------------------------- Resumo da operação -------------------------\n\n");
+    printf("Processos aguardando: %d\n", countWaitingCustomers(customers));
+    printf("\nProcessos em atendimento: %d\n", countAttendCustomers(customers));
+    printf("\nProcessos finalizados: %d\n", countAlreadyAttendCustomers(customers));
+    Sleep(DEFAULT_PAUSE * MILLISECOND);
+    system("cls");
+}
+
+
